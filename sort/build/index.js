850ce4e99b8b355053d552e568935fe74bc48b9b
Heriis <cpalau23@gmail.com>
2022-10-13 13:59:49 +0200

    sort

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('HasWrbLXz');
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(charactersCollection.data);
